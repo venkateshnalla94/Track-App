024931715efe5e61e10003c4d8e9c87c2baf9bcd
Venkatesh <venkateshnalla94@gmail.com>
2020-10-12 04:20:45 +0530

    Spacer Component to add margin around the react elements. AuthContext.js introduced to have actions on state change. createDataContext.js introduced to have a centralized layer for context. SignupScreen.js styling has been changed. React native elements dependency installed

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
-
+import {Provider as AuthProvider} from "./src/context/AuthContext";
 
 const switchNavigator=createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -26,13 +26,11 @@ const switchNavigator=createSwitchNavigator({
   })
 })
 
-export default createAppContainer(switchNavigator)
+const App = createAppContainer(switchNavigator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return <AuthProvider>
+    <App/>
+  </AuthProvider>
+};
+
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,55 @@
-import React from 'react' ;
-import {View,Text,StyleSheet} from "react-native";
+import React,{useState} from 'react' ;
+import {View,StyleSheet,} from "react-native";
+import {Text,Input,Button} from 'react-native-elements'
+import Spacer from "../components/Spacer";
 
-const SignupScreen=()=>{
+const SignupScreen=({navigation})=>{
+     const [email,setEmail]=useState('');
+     const [password,setPassword]=useState('')
+     
      return (
-          <View>
-               <Text>SignUp</Text>
+          <View style={styles.parentStyle}>
+               <Text style={styles.headerStyle} h4>Sign Up For Tracker</Text>
+               <Input
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    label={'Email'}
+                    value={email}
+                    onChangeText={setEmail}
+               />
+               <Spacer/>
+               <Input
+                    secureTextEntry={true}
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    label={'Password'}
+                    value={password}
+                    onChangeText={setPassword}
+               />
+               <Spacer>
+                    <Button title={'Sign In'} onPress={()=>{navigation.navigate('Signin')}}/>
+               </Spacer>
+               
+               {/*<Button title={'Main Flow'} onPress={()=>{navigation.navigate('mainFlow')}}/>*/}
           </View>
      )
 }
+SignupScreen.navigationOptions = () => {
+     return {
+          headerShown: false,
+     };
+};
 
 const styles=StyleSheet.create({
-
+     headerStyle:{
+          margin:20,
+          textAlign:'center'
+     },
+     parentStyle:{
+          flex:1,
+          justifyContent:'center',
+          marginBottom:150
+     }
 })
 
 export default SignupScreen;
